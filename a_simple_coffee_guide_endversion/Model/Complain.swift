94db2a26f350a6983d10e661922f0174d91174ce
sketchup94 <uemitgul@gmail.com>
2019-04-19 17:38:04 +0200

    Fixed the mysterious lose of all my models

---
+++ a_simple_coffee_guide_endversion/Model/Complain.swift
@@ -0,0 +1,15 @@
+//
+//  Complain.swift
+//  a_simple_coffee_guide_endversion
+//
+//  Created by Ümit Gül on 19.04.19.
+//  Copyright © 2019 Ümit Gül. All rights reserved.
+//
+
+import Foundation
+
+struct Complain {
+
+    let complain : String
+    let improvements : [String]
+}
---
+++ a_simple_coffee_guide_endversion/Model/KnowledgeModel.swift
@@ -0,0 +1,17 @@
+//
+//  KnowledgeModel.swift
+//  a_simple_coffee_guide_endversion
+//
+//  Created by Ümit Gül on 19.04.19.
+//  Copyright © 2019 Ümit Gül. All rights reserved.
+//
+
+import Foundation
+
+struct Knowledge {
+    
+    let name : String
+    let imageName : String
+    let tips : [String]
+}
+
