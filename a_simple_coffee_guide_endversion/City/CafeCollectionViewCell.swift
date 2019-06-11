684eea8ac85ca1f55fe8c1aad1732741c67b121d
sketchup94 <uemitgul@gmail.com>
2019-06-11 19:06:39 +0200

    Add CollectingViewCell - but not finished

---
+++ a_simple_coffee_guide_endversion/City/CafeCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CafeCollectionViewCell.swift
+//  a_simple_coffee_guide_endversion
+//
+//  Created by Ümit Gül on 11.06.19.
+//  Copyright © 2019 Ümit Gül. All rights reserved.
+//
+
+import UIKit
+
+class CafeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cafeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.cornerRadius = 8
+    }
+}
